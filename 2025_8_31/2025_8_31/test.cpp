ad127668e60ac8e5a90f60f1dc929342a7415c66
Zyf_de_github <3397337213@qq.com>
2025-09-01 16:55:25 +0800

    leetcode

--- 2025_8_31/2025_8_31/test.cpp
+++ 2025_8_31/2025_8_31/test.cpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 
 
-class Solution {
+class Solution1 {
 private:
     struct ListNode 
     {
@@ -77,17 +78,90 @@ public:
         }
         return false;
     }
-
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> myMap;
+        int size = nums.size();
+        for (auto it : nums)
+        {
+            if ((++myMap[it]) > size / 2)return it;
+        }
+        return 0;
+    }
+    void nextPermutation(vector<int>& nums) {
+        int size = nums.size();
+        int mid = 0;
+        for (int i = size - 1 - 1; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                mid = i;
+                break;
+            }
+            if (i == 0)
+            {
+                sort(nums.begin(), nums.end());
+                return;
+            }
+        }
+        //找后面所有比mid大的最小的数
+        int j = size - 1;
+        while (j)
+        {
+            if (nums[j] > nums[mid])break;
+            j--;
+        }
+        swap(nums[j], nums[mid]);
+        sort(nums.begin() + mid+1, nums.end());
+        return;
+    }
 };
 
-
+class Solution {
+private:
+    vector<vector<int>> result;
+    int size = 0;
+public:
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+#if 0
+        sort(nums.begin(), nums.end());
+#endif
+        size = nums.size();
+        finding(nums, {});
+        return result;
+    }
+    void finding(vector<int> nums, vector<int> choose)
+    {
+        if (nums.size() == 0)
+        {
+            result.push_back(choose);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+#if 0 //开关，打开是去重版本
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+#endif
+            vector<int> temp = nums;//传入待选数组
+            vector<int> x = choose;
+            x.push_back(temp[i]);
+            temp.erase(temp.begin() + i);
+            finding(temp, x);
+        }
+    }
+};
 
 
 int main()
 {
-    vector<vector<int>> a = { {1,3},{2,6},{8,10},{15,18} };
-    Solution test;
-    test.merge(a);
+    //vector<vector<int>> a = { {1,3},{2,6},{8,10},{15,18} };
+    //vector<int> a = { 1,2,3 };
+    //Solution test;
+    ////test.merge(a);
+    //test.nextPermutation(a);
+
+
+
 	return 0;
 }
 
