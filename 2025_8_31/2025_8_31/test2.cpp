9f185955b09f8ed163a4ce9f87c4e6f56cdb8f10
Zyf_de_github <3397337213@qq.com>
2025-09-09 20:58:48 +0800

    leetcode

--- 2025_8_31/2025_8_31/test2.cpp
+++ 2025_8_31/2025_8_31/test2.cpp
@@ -99,6 +99,32 @@ public:
         if (j - 1 >= 0 && grid[i][j - 1] == 1) { grid[i][j - 1] = 3; change = true; }
     }
 };
+class Solution3 {
+    vector<string> s;
+public:
+    vector<string> validStrings(int n)
+    {
+        finding(n, "", 0);
+        return s;
+    }
+    void finding(int n, string str, int num)
+    {
+        if (n == 0)
+        {
+            s.push_back(str);
+            return;
+        }
+        if (num == 1)
+        {
+            finding(n - 1, str + '1', 0);
+        }
+        else if (num == 0)
+        {
+            finding(n - 1, str + '1', 0);
+            finding(n - 1, str + '0', 1);
+        }
+    }
+};
 class Solution 
 {
 private:
@@ -147,7 +173,80 @@ public:
         }
         return head;
     }
-
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        vector<int> hang;
+        vector<int> lie;
+        int size = grid.size();
+        for (int i = 0; i < size; i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < size; j++)
+            {
+                temp = max(temp, grid[i][j]);
+            }
+            hang.push_back(temp);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < size; j++)
+            {
+                temp = max(temp, grid[j][i]);
+            }
+            lie.push_back(temp);
+        }
+        int ans = 0;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                ans += min(hang[i], lie[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> ans;
+        for (int i = 0; i < queries.size(); i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < points.size(); j++)
+            {
+                if ((queries[i][0] - points[j][0]) * (queries[i][0] - points[j][0]) + (queries[i][1] - points[j][1]) * (queries[i][1] - points[j][1]) <= queries[i][2] * queries[i][2])temp++;
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+    vector<int> minOperations(string boxes) {
+        vector<int> v;
+        int size = boxes.size();
+        for (int i = 0; i < size; i++)
+        {
+            int ans = 0;
+            for (int j = 0; j < size; j++)
+            {
+                if (boxes[j] == '1')
+                {
+                    ans += abs(j - i);
+                }
+            }
+            v.push_back(ans);
+        }
+        return v;
+    }
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        int times = 0;
+        while (times < nums.size())
+        {
+            ans.push_back(nums[times + 1]);
+            ans.push_back(nums[times]);
+            times += 2;
+        }
+        return ans;
+    }
 };
 
 class MyCircularQueue2 {
