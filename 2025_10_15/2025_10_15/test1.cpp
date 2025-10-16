4eb8b0361bc078024946493f4920bffeee293d6b
Zyf_de_github <3397337213@qq.com>
2025-10-16 13:40:47 +0800

    leetcode

--- 2025_10_15/2025_10_15/test1.cpp
+++ 2025_10_15/2025_10_15/test1.cpp
@@ -115,6 +115,144 @@ public:
         return !((x != 0 || y != 0) && state == 0);
     }
 };
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int ans = 0;
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < accounts[0].size(); j++)
+            {
+                temp += accounts[i][j];
+            }
+            ans = max(ans, temp);
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int ans = 0;
+        int size = mat.size();
+        for (int i = 0; i < size; i++)
+        {
+            ans += mat[i][i];
+            ans += mat[i][size - 1 - i];
+        }
+        if (size % 2)ans -= mat[size / 2][size / 2];
+        return ans;
+    }
+};
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+};
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int nmax = INT_MIN;
+        int nmin = INT_MAX;
+        int ans = 0;
+        for (auto it : salary)
+        {
+            nmin = min(nmin, it);
+            nmax = max(nmax, it);
+            ans += it;
+        }
+        return (ans - nmin - nmax) / (salary.size() - 2.0);
+    }
+};
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int m5 = 0;
+        int m10 = 0;
+        int m20 = 0;
+        for (auto it : bills)
+        {
+            if (it == 5)m5++;
+            else if (it == 10)
+            {
+                m10++;
+                m5--;
+            }
+            else
+            {
+                m20++;
+                if (m10 > 0)
+                {
+                    m10--;
+                    m5--;
+                }
+                else
+                {
+                    m5 -= 3;
+                }
+            }
+            if (m5 < 0 || m10 < 0 || m20 < 0)return false;
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        for (int i = size - 1; i >= 2; i--)
+        {
+            if (nums[i - 1] + nums[i - 2] > nums[i])return nums[i - 1] + nums[i - 2] + nums[i];
+        }
+        return 0;
+    }
+};
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        if (coordinates[0][0] == coordinates[1][0])
+        {
+            for (int i = 2; i < coordinates.size(); i++)
+            {
+                if (coordinates[i][0] != coordinates[0][0])return false;
+            }
+            return true;
+        }
+        if (coordinates[0][1] == coordinates[1][1])
+        {
+            for (int i = 2; i < coordinates.size(); i++)
+            {
+                if (coordinates[i][1] != coordinates[0][1])return false;
+            }
+            return true;
+        }
+        double x1 = coordinates[0][0];
+        double y1 = coordinates[0][1];
+        double x2 = coordinates[1][0];
+        double y2 = coordinates[1][1];
+        double k = (y1 - y2) / (x1 - x2);
+        double b = y1 - k * x1;
+        for (int i = 2; i < coordinates.size(); i++)
+        {
+            if (coordinates[i][1] != k * coordinates[i][0] + b)return false;
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
 
