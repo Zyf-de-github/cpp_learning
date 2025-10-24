93e5bbfa969149609ef12f595fb8475ed161c2d7
Zyf_de_github <3397337213@qq.com>
2025-10-24 15:30:21 +0800

    tree

--- 2025_10_15/2025_10_15/dp.cpp
+++ 2025_10_15/2025_10_15/dp.cpp
@@ -14,6 +14,40 @@
 #include <random>
 using namespace std;
 
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution0 {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.empty())return NULL;
+        int x = 0;
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            if (preorder[0] == inorder[i])
+            {
+                x = i;
+                break;
+            }
+        }
+        TreeNode* head = new TreeNode(preorder[0]);
+        vector<int> preorder_left(preorder.begin() + 1, preorder.begin() + x + 1);
+        vector<int> preorder_right(preorder.begin() + x + 1, preorder.end());
+
+        vector<int> inorder_left(inorder.begin(), inorder.begin() + x);
+        vector<int> inorder_right(inorder.begin() + x + 1, inorder.end());
+        head->left = buildTree(preorder_left, inorder_left);
+        head->right = buildTree(preorder_right, inorder_right);
+        return head;
+    }
+};
 class Solution1 {
 public:
     vector<long long>memo;
@@ -82,6 +116,66 @@ public:
         return ans;
     }
 };
+class Solution4 {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        root->right = left;
+        root->left = nullptr;
+
+        TreeNode* p = root;
+        while (p->right) p = p->right;
+
+        p->right = right;
+    }
+};
+class Solution5 {
+public:
+    vector<int>v;
+    vector<int> rightSideView(TreeNode* root) {
+        int n = 1;
+        finding(root, n);
+        return v;
+    }
+    void finding(TreeNode* root, int n)
+    {
+        if (root == nullptr)return;
+        if (v.size() < n)v.push_back(root->val);
+        finding(root->right, n + 1);
+        finding(root->left, n + 1);
+    }
+};
+class Solution6 {
+public:
+    vector<int> v;
+    int n = 0;
+    int kthSmallest(TreeNode* root, int k) {
+        finding(root, k);
+        return v[k - 1];
+    }
+    void finding(TreeNode* root, int& k)
+    {
+        if (root == nullptr)return;
+        if (n != k)finding(root->left, k);
+        v.push_back(root->val);
+        n++;
+        if (n != k)finding(root->right, k);
+    }
+};
+class Solution7 {
+public:
+    bool isValidBST(TreeNode* root, long long left = LLONG_MIN, long long right = LLONG_MAX) {
+        if (root == nullptr)return true;
+        long long x = root->val;
+        return x > left && x < right && isValidBST(root->left, left, x) && isValidBST(root->right, x, right);
+    }
+};
+
+
 
 int main()
 {
