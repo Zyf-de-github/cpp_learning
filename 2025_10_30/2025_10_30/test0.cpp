9a6fc94456230d95f2e246115654964a58cdcd7a
Zyf_de_github <3397337213@qq.com>
2025-11-03 21:37:18 +0800

    leetcode

--- 2025_10_30/2025_10_30/test0.cpp
+++ 2025_10_30/2025_10_30/test0.cpp
@@ -14,7 +14,7 @@
 #include <random>
 using namespace std;
 
-class Solution {
+class Solution0 {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
@@ -53,8 +53,84 @@ public:
         }
         return ans;
     }
+// https://www.bilibili.com/video/BV1gN411E7Zx/?spm_id_from=333.337.search-card.all.click&vd_source=b5cc04f324fc9d6ee48a5febd77392fc
 };
 
+struct ListNode 
+ {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+class Solution2 {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* before = new ListNode();
+        before->next = head;
+        head = before;
+        for (int x = 1; x < left; x++)
+        {
+            before = before->next;
+        }
+        ListNode* list = new ListNode();
+        ListNode* last = before->next;
+        int time = right;
+        while (time <= left)
+        {
+            list->val = last->val;
+            ListNode* temp = new ListNode();
+            temp->next = list;
+            list = temp;
+            last = last->next;
+            time++;
+        }
+        before->next = list->next;
+        while (before->next)
+        {
+            before = before->next;
+        }
+        before->next = last;
+        return head->next;
+    }
+};
+class Solution3 {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k)
+    {
+        ListNode* temp = new ListNode(0, head);
+        ListNode* find = temp;
+        head = temp;
+        while (true)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                find = find->next;
+                if (find == nullptr)return head->next;
+            }
+            reverseBetween(temp, k);
+            for (int i = 0; i < k; i++)
+            {
+                temp = temp->next;
+            }
+            find = temp;
+        }
+        return head->next;
+    }
+    void reverseBetween(ListNode* pre, int k)
+    {
+        ListNode* cur = pre->next;
+        for (int i = 0; i < k - 1; i++)
+        {
+            ListNode* next = cur->next;
+            cur->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+        }
+        return;
+    }
+};
 int main()
 {
     Solution1 s;
