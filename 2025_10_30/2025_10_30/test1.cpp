ba996c910624df2fd6cf9260f95f613ce82bc148
Zyf_de_github <3397337213@qq.com>
2025-11-23 12:06:25 +0800

    leetcode

--- 2025_10_30/2025_10_30/test1.cpp
+++ 2025_10_30/2025_10_30/test1.cpp
@@ -101,8 +101,9 @@ public:
         int n = nums.size();
         vector<long long>ans;
         vector<vector<int>> per;
+
         int temp = 0;
-        for (int i = 1; i < nums.size(); i++)
+        for (int i = 1; i < n; i++)
         {
             if (nums[i] < nums[i - 1])
             {
@@ -110,35 +111,161 @@ public:
                 temp = i;
             }
         }
-        per.push_back({ temp,n - 1 });
+        per.push_back({ temp,n - 1 });//所有单调递增区间下标
 
 
         for (int i = 0; i < queries.size(); i++)
         {
             long long add_num = 0;
-            int j = 0;
-            while (j < per.size() && per[j][0] < queries[i][1])
+            temp = 0;
+            int lptr = 0;
+            int rptr = per.size() - 1;
+            while (lptr <= rptr)
             {
-                if (per[j][1] < queries[i][0])
-                {
-                    j++;
-                    continue;
-                }
-                long long x = min(queries[i][1], per[j][1]) - max(queries[i][0], per[j][0]) + 1;
+                int mid = (lptr + rptr) / 2;
+                if (per[mid][1] >= queries[i][0]) rptr = mid - 1;
+                else lptr = mid + 1;
+            }
+            temp = lptr;
+            while (temp < per.size() && per[temp][0] <= queries[i][1])
+            {
+                long long x = min(queries[i][1], per[temp][1]) - max(queries[i][0], per[temp][0]) + 1;
                 add_num += x * (x + 1) / 2;
-                j++;
+                temp++;
             }
             ans.push_back(add_num);
         }
         return ans;
     }
-};   
+};
+class Solution6 {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n, -1);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            int h = heights[i];
+            while (!st.empty() && heights[st.top()] >= h) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                left[i] = st.top();
+            }
+            st.push(i);
+        }
+
+        vector<int> right(n, n);
+        st = stack<int>();
+        for (int i = n - 1; i >= 0; i--) {
+            int h = heights[i];
+            while (!st.empty() && heights[st.top()] >= h) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                right[i] = st.top();
+            }
+            st.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, heights[i] * (right[i] - left[i] - 1));
+        }
+        return ans;
+    }
+};
+
+class Solution7 {
+public:
+    long long sumAndMultiply(int n) {
+        string s = to_string(n);
+        string x_;
+        long long sum_ = 0;
+        for (auto it : s)
+        {
+            if (it != '0')
+            {
+                x_ = x_ + string(1, it);
+                sum_ += it - '0'; 
+            }
+        }
+        if (!x_.size())return 0;
+        return stoi(x_) * sum_;
+    }
+};  
+class Solution8 {
+public:
+    struct PairHash
+    {
+        size_t operator()(const pair<unsigned int, int>& p) const noexcept
+        {
+            return hash<unsigned int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
+
+    int maxBalancedSubarray(vector<int>& nums) {
+        int n = nums.size();
+        vector<unsigned int> XOR(n, 0);
+        int sin = 0, dou = 0;
+        vector<int>equal(n, 0);
+        XOR[0] = nums[0];
+        nums[0] % 2 ? sin += 1 : dou += 1;
+        equal[0] = sin - dou;
+        for (int i = 1; i < n; i++)
+        {
+            nums[i] % 2 ? sin += 1 : dou += 1;
+            equal[i] = sin - dou;
+            XOR[i] = XOR[i - 1] ^ nums[i];
+        }
+        unordered_map<pair<unsigned int, int>, int, PairHash> m;
+        m[{0, 0}] = -1;
+        int length = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (m.count({ XOR[i], equal[i] }))
+            {
+                length = max(length, i - m[{XOR[i], equal[i]}]);
+            }
+            else m[{XOR[i], equal[i]}] = i;
+        }
+        return length;
+    }
+}; 
+class Solution {
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries)
+    {
+        int n = s.size();
+        vector<long long> num_sum;
+        vector<string> num_x;
+        long long temp_sum = 0; string temp_x;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] != '0')
+            {
+                temp_x = temp_x + s[i];
+                temp_sum += s[i] - '0';
+                temp_sum %= 1000000007;
+                num_sum.push_back(temp_sum);
+                num_x.push_back(temp_x);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < queries.size(); i++)
+        {
+            string temp = num_x[queries[i][1]].substr(num_x[queries[i][0]].size());
+            ans.push_back((stol(temp) % 1000000007 * (num_sum[queries[i][1]] - num_sum[queries[i][0]])) % 1000000007);
+        }
+        return ans;
+    }
+}; 
 int main()
 {
-    Solution5 s;
-    vector<int>  nums = { 3,1,2 };
+    Solution8 s;
+    vector<int>  nums = { 0 };
     vector<vector<int>>  queries = { {0,1},{1,2},{0,2} };
-    s.countStableSubarrays(nums, queries);
+    cout<<s.maxBalancedSubarray(nums);
 	return 0;
 }
 
