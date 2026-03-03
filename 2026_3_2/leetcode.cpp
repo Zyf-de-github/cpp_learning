8f057ac0db50445123ab731b5ed6d3323acd332f
Zyf_de_github <3397337213@qq.com>
2026-03-03 11:45:40 +0800

    leetcode

--- 2026_3_2/leetcode.cpp
+++ 2026_3_2/leetcode.cpp
@@ -1,105 +1,105 @@
-#include <algorithm>
-#include <ctime>
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-class Solution1 {
-public:
-    int search(vector<int>& nums, int target) {
-        int left=0,right=nums.size()-1;
-        while(left<right)
-        {
-            int mid=(left+right)/2;
-            if (nums[mid]>nums.back())left=mid+1;
-            else right=mid;
-        }
-        if (target<nums[right])return -1;
-        else
-        {
-            if (target>=nums[right]&&target<=nums.back())
-            {
-                return dev_search(nums,target,right,nums.size()-1);
-            }
-            else
-            {
-                return dev_search(nums,target,0,(right-1)<0?0:right-1);
-            }
-        }
-    }
-    int dev_search(vector<int>& nums, int target,int left,int right)
-    {
-        while (left<right)
-        {
-            int mid=(left+right)/2;
-            if (nums[mid]<target)left=mid+1;
-            else if (nums[mid]>target)right=mid;
-            else if (nums[mid]==target)return mid;
-        }
-        if (nums[left]==target)return left;
-        return -1;
-    }
-
-
-
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end(),[&](int a,int b){
-            return a>b;
-        });
-        return nums[k-1];
-    }
-
-
-};
-class Solution {
-public:
-    vector<int> sortArray(vector<int>& nums) {
-        std::srand(std::time(nullptr));
-        fastSort(nums,0,nums.size()-1);
-        return nums;
-    }
-
-    void fastSort(vector<int>& nums,int left,int right)
-    {
-        if(right-left<=0)return;
-        int randomIndex = left + std::rand() % (right - left + 1);
-        int pivot = nums[randomIndex];
-        swap(nums[left],nums[randomIndex]);
-        int lptr=left,rptr=right,key=1;
-        while(lptr!=rptr)
-        {
-            if(key)
-            {
-                if(nums[rptr]<pivot)
-                {
-                    swap(nums[lptr],nums[rptr]);
-                    lptr++;
-                    key=0;
-                }
-                else rptr--;
-            }
-            else
-            {
-                if(nums[lptr]>pivot)
-                {
-                    swap(nums[lptr],nums[rptr]);
-                    rptr--;
-                    key=1;
-                }
-                else lptr++;
-            }
-        }
-        fastSort(nums,left,lptr-1);
-        fastSort(nums,lptr+1,right);
-    }
-};
-
-int main()
-{
-    Solution s;
-    vector<int> nums = {2,2,2,2,2,2,2,2,2};
-    s.sortArray(nums);
-
-    return 0;
-}
+// #include <algorithm>
+// #include <ctime>
+// #include <iostream>
+// #include <vector>
+//
+// using namespace std;
+//
+// class Solution1 {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int left=0,right=nums.size()-1;
+//         while(left<right)
+//         {
+//             int mid=(left+right)/2;
+//             if (nums[mid]>nums.back())left=mid+1;
+//             else right=mid;
+//         }
+//         if (target<nums[right])return -1;
+//         else
+//         {
+//             if (target>=nums[right]&&target<=nums.back())
+//             {
+//                 return dev_search(nums,target,right,nums.size()-1);
+//             }
+//             else
+//             {
+//                 return dev_search(nums,target,0,(right-1)<0?0:right-1);
+//             }
+//         }
+//     }
+//     int dev_search(vector<int>& nums, int target,int left,int right)
+//     {
+//         while (left<right)
+//         {
+//             int mid=(left+right)/2;
+//             if (nums[mid]<target)left=mid+1;
+//             else if (nums[mid]>target)right=mid;
+//             else if (nums[mid]==target)return mid;
+//         }
+//         if (nums[left]==target)return left;
+//         return -1;
+//     }
+//
+//
+//
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end(),[&](int a,int b){
+//             return a>b;
+//         });
+//         return nums[k-1];
+//     }
+//
+//
+// };
+// class Solution {
+// public:
+//     vector<int> sortArray(vector<int>& nums) {
+//         std::srand(std::time(nullptr));
+//         fastSort(nums,0,nums.size()-1);
+//         return nums;
+//     }
+//
+//     void fastSort(vector<int>& nums,int left,int right)
+//     {
+//         if(right-left<=0)return;
+//         int randomIndex = left + std::rand() % (right - left + 1);
+//         int pivot = nums[randomIndex];
+//         swap(nums[left],nums[randomIndex]);
+//         int lptr=left,rptr=right,key=1;
+//         while(lptr!=rptr)
+//         {
+//             if(key)
+//             {
+//                 if(nums[rptr]<pivot)
+//                 {
+//                     swap(nums[lptr],nums[rptr]);
+//                     lptr++;
+//                     key=0;
+//                 }
+//                 else rptr--;
+//             }
+//             else
+//             {
+//                 if(nums[lptr]>pivot)
+//                 {
+//                     swap(nums[lptr],nums[rptr]);
+//                     rptr--;
+//                     key=1;
+//                 }
+//                 else lptr++;
+//             }
+//         }
+//         fastSort(nums,left,lptr-1);
+//         fastSort(nums,lptr+1,right);
+//     }
+// };
+//
+// int main()
+// {
+//     Solution s;
+//     vector<int> nums = {2,2,2,2,2,2,2,2,2};
+//     s.sortArray(nums);
+//
+//     return 0;
+// }
---
+++ 2026_3_2/leetcode_3_3.cpp
@@ -0,0 +1,78 @@
+#include <climits>
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution1 {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(INT_MIN,head);
+        ListNode* ptr=head;
+        ListNode* pre_ptr=dummy;
+        while(ptr)
+        {
+            ListNode* temp=dummy;
+            while(temp->next->val<ptr->val)
+            {
+                temp=temp->next;
+            }
+            if(temp->next->val>=ptr->val&&temp->next!=ptr)
+            {
+                pre_ptr->next=ptr->next;
+                ptr->next=temp->next;
+                temp->next=ptr;
+                ptr=pre_ptr->next;
+            }
+            else
+            {
+                pre_ptr=ptr;
+                ptr=ptr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
+class Solution {
+public:
+    int numberOfBeams(vector<std::string>& bank) {
+        int ans=0,last_r=0;
+        for(int i=0;i<bank.size();i++)
+        {
+            int temp=0;
+            for(auto it:bank[i])
+            {
+                if(it=='1')temp++;
+            }
+            if(temp)
+            {
+                ans+=last_r*temp;
+                last_r=temp;
+            }
+        }
+        return ans;
+    }
+};
+
+
+int main()
+{
+    // Solution s;
+    // ListNode* a = new ListNode(3);
+    // ListNode* b = new ListNode(3,a);
+    // ListNode* c = new ListNode(1,b);
+    // ListNode* d = new ListNode(2,c);
+    // ListNode* head = new ListNode(4,d);
+    // s.insertionSortList(head);
+    return 0;
+}
