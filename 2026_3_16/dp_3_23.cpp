6f14a674b7314560b6d979a5469dd7c316adcbd9
Zyf_de_github <3397337213@qq.com>
2026-03-25 20:32:58 +0800

    dp

--- 2026_3_16/dp_3_23.cpp
+++ 2026_3_16/dp_3_23.cpp
@@ -2,7 +2,7 @@
 #define NUM 1000000007
 using namespace std;
 
-class Solution1 {
+class Solution11 {
 public:
     // int maximumCostSubstring(string s, string chars, vector<int>& vals) {
     //     map<char,int> m;
@@ -32,7 +32,7 @@ public:
         return ans;
     }
 };
-class Solution2 {
+class Solution12 {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
         int nums_size=nums.size();
@@ -46,7 +46,7 @@ public:
         return -min_ans>max_ans ? -min_ans : max_ans;
     }
 };
-class Solution3 {
+class Solution13 {
 public:
     int kConcatenationMaxSum(vector<int>& arr, int k) {
         if (k<3) return k_times(arr,k);
@@ -90,7 +90,7 @@ public:
         return num_max;
     }
 };
-class Solution4 {
+class Solution14 {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
         int nums_size=nums.size();
@@ -106,7 +106,7 @@ public:
         return max_nums<0?max_nums:max(total-min_ans,max_ans);
     }
 };
-class Solution5 {
+class Solution15 {
 public:
     bool canPartition(vector<int>& nums) {
         int sum=0;
@@ -125,7 +125,7 @@ public:
         return memo[nums.size()][sum];
     }
 };
-class Solution6 {
+class Solution16 {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum=0;
@@ -148,7 +148,7 @@ public:
     }
 
 };
-class Solution {
+class Solution17 {
 public:
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
         vector<vector<pair<bool,int>>> memo(nums.size()+1,vector<pair<bool,int>>(target+1,{false,0}));
@@ -170,11 +170,123 @@ public:
         return memo[nums.size()][target].first==true?memo[nums.size()][target].second:-1;
     }
 };
+class Solution18 {
+public:
+    int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        vector<int> sub1_2(n,0);
+        vector<int> sub2_1(n,0);
+        int sum1=0,sum2=0;
+        for (int i=0;i<n;i++) {
+            sub1_2[i]=nums1[i]-nums2[i];
+            sub2_1[i]=nums2[i]-nums1[i];
+            sum1+=nums1[i];
+            sum2+=nums2[i];
+        }
+        int ans1=sum1+func(sub2_1);
+        int ans2=sum2+func(sub1_2);
+        return max(ans1,ans2);
+    }
+    int func(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n+1,0);
+        int sum=0;
+        for (int i=1;i<=n;i++) {
+            sum+=nums[i-1];
+            if (sum<=0) {
+                sum=0;
+                dp[i]=dp[i-1];
+            }
+            else {
+                dp[i]=max(sum,dp[i-1]);
+            }
+        }
+        return dp[n];
+    }
+};
+class Solution19 {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int max_num=nums[0];
+        for (auto it:nums)max_num=max(max_num,it);
+        if (max_num<=0)return max_num;
+        vector<int> dp(n+1,0);
+        int sum=0;
+        for (int i=1;i<=n;i++) {
+            sum+=nums[i-1];
+            if (sum<=0) {
+                sum=0;
+                dp[i]=dp[i-1];
+            }
+            else {
+                dp[i]=max(dp[i-1],sum);
+            }
+        }
+        return dp[n];
+    }
+};
+class Solution1 {
+public:
+    int countPathsWithXorValue(vector<vector<int>>& grid, int a) {
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<vector<int>>> v(m, vector<vector<int>>(n, vector<int>(16, 0)));
+        v[0][0][grid[0][0]]=1;
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                for (int k=0;k<16;k++) {
+                    if (v[i][j][k]) {
+                        if (i==m-1) {
+                            if (j<n-1) {
+                                v[i][j+1][k^grid[i][j+1]]+=v[i][j][k];
+                                v[i][j+1][k^grid[i][j+1]]%=NUM;
+                            }
+                        }
+                        else if (j==n-1) {
+                            v[i+1][j][k^grid[i+1][j]]+=v[i][j][k];
+                            v[i+1][j][k^grid[i+1][j]]%=NUM;
+                        }
+                        else {
+                            v[i+1][j][k^grid[i+1][j]]+=v[i][j][k];
+                            v[i+1][j][k^grid[i+1][j]]%=NUM;
+                            v[i][j+1][k^grid[i][j+1]]+=v[i][j][k];
+                            v[i][j+1][k^grid[i][j+1]]%=NUM;
+                        }
+                    }
+                }
+            }
+        }
+        return v[m-1][n-1][a];
+    }
+};
+class Solution2 {
+public:
+    long long minCost(int m, int n, vector<vector<int>> waitCost) {
+        vector<vector<long long>> dp(m,vector<long long>(n,0));
+        dp[0][0]=1;
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (i==0&&j==0)continue;
+                if (i==0) {
+                    dp[i][j]=dp[i][j-1]+waitCost[i][j]+(i+1)*(j+1);
+                }
+                else if (j==0) {
+                    dp[i][j]=dp[i-1][j]+waitCost[i][j]+(i+1)*(j+1);
+                }
+                else {
+                    dp[i][j]=min(dp[i][j-1],dp[i-1][j])+waitCost[i][j]+(i+1)*(j+1);
+                }
+            }
+        }
+        return dp[m-1][n-1]-waitCost[m-1][n-1];
+    }
+};
 
 int main() {
     Solution s;
-    vector<int> v={1,1,5,4,5};
-    s.lengthOfLongestSubsequence(v,3);
+    vector<int> v1={60,60,60},v2={10,90,10};
+    vector<vector<int>> v3={{2,1,5},{7,10,0},{12,6,4}};
+    s.minCost(1,2,{{1,2}});
 
     return 0;
 }
